996c5f22cd3804b7b2baab1f532f6d880db7b39f
Madiyar <mmadi8218@example.com>
2026-03-14 17:53:23 +0600

    Обновил логику работы приложения

--- vite.config.ts
+++ vite.config.ts
@@ -39,6 +39,25 @@ export default defineConfig(({mode}) => {
             }
           ]
         },
+        workbox: {
+          globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+          runtimeCaching: [
+            {
+              urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'google-fonts-cache',
+                expiration: {
+                  maxEntries: 10,
+                  maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+                },
+                cacheableResponse: {
+                  statuses: [0, 200],
+                },
+              },
+            },
+          ],
+        },
         devOptions: {
           enabled: true
         }
